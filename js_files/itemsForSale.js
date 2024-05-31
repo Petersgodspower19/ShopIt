ba37e4c18bb17f1d296033fcdc1f19b72ed384bb
Peters Godspower Chidiebube <89461406+Petersgodspower19@users.noreply.github.com>
2024-05-31 14:43:04 +0100

    Update itemsForSale.js

--- js_files/itemsForSale.js
+++ js_files/itemsForSale.js
@@ -35,7 +35,81 @@ export const itemsForSale = {
       },
       {
         percentageOff: "20%",
+        image: "./woman-shoe.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "women"
+      },
+      {
+        percentageOff: "20%",
+        image: "./woman-shoe.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "women"
+      },
+      {
+        percentageOff: "20%",
+        image: "./woman-shoe.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "women"
+      },
+      {
+        percentageOff: "20%",
+        image: "./woman-shoe.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "women"
+      },
+    
+
+      {
+        percentageOff: "20%",
         image: "./watch-home.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "men"
+      },
+      {
+        percentageOff: "20%",
+        image: "./watch-home.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "men"
+      },
+      {
+        percentageOff: "20%",
+        image: "./watch-home.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "men"
+      },
+      {
+        percentageOff: "20%",
+        image: "./watch-home.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "men"
+      },
+      {
+        percentageOff: "20%",
+        image: "./aF1-home.jpeg",
+        description: "Lorem ipsum dolor sit amet, consectetur.",
+        newprice: "&euro; 960",
+        oldprice: "&euro; 1200",
+        Isfor: "men"
+      },
+      {
+        percentageOff: "20%",
+        image: "./aF1-home.jpeg",
         description: "Lorem ipsum dolor sit amet, consectetur.",
         newprice: "&euro; 960",
         oldprice: "&euro; 1200",
